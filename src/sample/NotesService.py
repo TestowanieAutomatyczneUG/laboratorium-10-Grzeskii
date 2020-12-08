49f6690057d04eb43578464353a000d0b38c8bb0
grzeski <grzegorz.rzeski@autonomik.pl>
2020-12-08 15:47:28 +0100

    added NotesService.py

---
+++ src/sample/NotesService.py
@@ -0,0 +1,18 @@
+from src.sample.NotesStorage import NotesStorage
+
+class NotesService:
+    def __init__(self):
+        self.notes_storage = NotesStorage()
+
+    def add(self, note):
+        self.notes_storage.add(note)
+
+    def averageOf(self, name):
+        all_notes = self.notes_storage.getAllNotesOf(name)
+        result = 0
+        for note in all_notes:
+            result += note.get_note()
+        return result / len(all_notes)
+
+    def clear(self):
+        self.notes_storage.clear()
